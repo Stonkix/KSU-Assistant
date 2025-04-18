759dc323539650238d2ea4911f31f42e5f527e0f
alexa <alexandr1lvl@gmail.com>
2025-04-18 22:30:02 +0300

    Файл для тг бота

--- Telegram-bot.py
+++ Telegram-bot.py
@@ -1,2 +1,10 @@
 import telebot
+bot = telebot.TeleBot("7637461107:AAFH6C5oy9WZIuQhZfkmH6YUbVNseduRA90") #Наш бот находится по тегу @tksu_bot
+
+@bot.message_handler(commands = ['start'])
+def say_hello(message):
+    bot.send_message(message.chat.id, 'Привет, я твой ассистент')
+
+bot.polling(none_stop = 'true')
+
 
