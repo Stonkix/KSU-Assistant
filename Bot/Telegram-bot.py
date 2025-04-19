1f9c915c8028d00d06b77c9f0341924a768a3b77
alexa <alexandr1lvl@gmail.com>
2025-04-19 17:17:41 +0300

    Виртуальное окружение

--- Bot/Telegram-bot.py
+++ Bot/Telegram-bot.py
@@ -1,10 +1,28 @@
 import telebot
-bot = telebot.TeleBot("7637461107:AAFH6C5oy9WZIuQhZfkmH6YUbVNseduRA90") #Наш бот находится по тегу @tksu_bot
+import sqlite3
+import os
+
+from telebot import types
+from Utils.Utils import hashPassword, verifyPassword
+
+dbDir = "C:/Users/alexa/Desktop/KSU-Assistant/Utils"
+DATABASE_PATH = os.path.join(dbDir, "university.db")
+botToken = '7637461107:AAFH6C5oy9WZIuQhZfkmH6YUbVNseduRA90'
+DATABASE_NAME = 'university.db'
+
+bot = telebot.TeleBot(botToken) # Наш бот находится по тегу @tksu_bot
+user_states = {} # Словарь для хранения временных данных пользователей во время авторизации
 
 @bot.message_handler(commands = ['start'])
 def say_hello(message):
     bot.send_message(message.chat.id, 'Привет, я твой ассистент')
 
-bot.polling(none_stop = 'true')
+def getDBConnection():
+    connect = sqlite3.connect(DATABASE_PATH)
+    connect.row_factory = sqlite3.Row
+    return connect
+
+
+bot.infinity_polling()
 
 
